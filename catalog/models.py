059b3690f127d856ef46b25d7171c65c2c2509ca
zakharchenkoantonina <158192333+zakharchenkoantonina@users.noreply.github.com>
2024-02-07 23:09:08 +0200

    first commit

---
+++ catalog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Customers(models.Model):
+    first name = models.ChartFild(max_length = 50)
+    last name = models.ChartFild(max_length = 50)
+    first name = models.ChartFild(max_length = 50)
+    first name = models.ChartFild(max_length = 50)
+    first name = models.ChartFild(max_length = 50)
+    first name = models.ChartFild(max_length = 50)
+    first name = models.ChartFild(max_length = 50)
+    created = models.DateTimeFild()
